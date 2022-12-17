0814ae7dfbe379f527f49afec72371b04fbcf3ab
Wojciech Nytko <nytkowo_aehit@students.vizja.pl>
2022-12-17 14:28:53 +0100

    docer

--- obj/Debug/netcoreapp3.1/BethanysPieShop.AssemblyInfo.cs
+++ obj/Debug/netcoreapp3.1/BethanysPieShop.AssemblyInfo.cs
@@ -12,6 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: Microsoft.AspNetCore.Identity.UI.UIFrameworkAttribute("Bootstrap4")]
+[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("88696380-ae7b-4500-87eb-67df849a5d16")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("BethanysPieShop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
